8f2281b856ce77c9a211e454e30058b888cb782c
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-26 13:25:34 +0600

    Initial Commit

---
+++ StreamFlix/StreamFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamFlixApp.swift
+//  StreamFlix
+//
+//  Created by Hafizur Rahman on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
