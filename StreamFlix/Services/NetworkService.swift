53cdd9cd51d139b825c4aa82801e6495a4c11c4d
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-26 15:12:07 +0600

    Network Service

---
+++ StreamFlix/Services/NetworkService.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+    case serverError(String)
+}
+
+class NetworkService {
+    static let shared = NetworkService()
+    private init() {}
+    
+    func fetch<T: Decodable>(from urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw NetworkError.serverError("Invalid response")
+        }
+        
+        do {
+            let decoded = try JSONDecoder().decode(T.self, from: data)
+            return decoded
+        } catch {
+            throw NetworkError.decodingError
+        }
+    }
+}
