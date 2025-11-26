ced429e9bb82b850614f48c602b7e40e8d19c17e
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-11-26 15:02:29 +0600

    Free HLS Video API

---
+++ StreamFlix/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Constants {
+    static let baseURL = "https://api.jsonbin.io/v3/b/" // We'll use JSONBin.io
+    
+        // Sample HLS video URLs (free test streams)
+    struct SampleVideos {
+        static let appleTestStream = "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8"
+        static let bigBuckBunny = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+        static let sintel = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+    }
+}
